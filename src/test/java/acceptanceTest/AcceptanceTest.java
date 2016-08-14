e30a0b361de9f9d295fb0b26dd0ea78270b083b6
Pascal Dufour <pascal@agilix.nl>
2016-08-14 23:32:26 +0200

    added a few test

--- src/test/java/acceptanceTest/AcceptanceTest.java
+++ src/test/java/acceptanceTest/AcceptanceTest.java
@@ -1,26 +1,55 @@
 package acceptanceTest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static java.lang.Thread.*;
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by pascaldufour on 11/08/16.
  */
+@Category(AcceptanceTest.class)
+
 public class AcceptanceTest {
 
+    WebDriver webDriver = null;
+    String serverURL = "http://localhost:8080/";
 
+    @Before
+    public void webDriver()
+
+    {
+        if (webDriver == null)
+            webDriver = new ChromeDriver();
+        else
+            webDriver.quit();
+        webDriver = new ChromeDriver();
+
+    }
 
     @Test
-    public void correctTitle() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080");
-        sleep(5000);  // Let the user actually see something!
+    public void hasTheMainPageTheCorrectTitle() throws InterruptedException {
+        webDriver.get(serverURL);
         assertEquals("Title should match",
-                "Apache Tomcat/8.0.36", driver.getTitle());
-        driver.quit();
-    };
+                "Apache Tomcat/8.0.36", webDriver.getTitle());
+    }
+
+
+     @Test
+    public void hasTheEmployeePageASearchOption() throws InterruptedException {
+        webDriver.get(serverURL + "/myPetProject/employee");
+         webDriver.findElement(By.id("seachEmployeeForm"));
+     }
+       @After
+    public void closeTheWebdriver() {
+           webDriver.close();
+           webDriver.quit();
+
+       }
+
 }
